3e9003ac17009826734cd0c3e3dd457610e4b8ca
Bryan Castillo <castillo.bryan@gmail.com>
2022-01-18 20:34:03 -0800

    Start an API and trying to call it from UX.

--- rookpromptux/src/FirstNavigation.tsx
+++ rookpromptux/src/FirstNavigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { SetMainViewAction, ActionDispatcher, MainViewType, BaseComponentProperties } from './AppActions';
 
-interface FirstNavigationParameters {
+interface FirstNavigationParameters extends BaseComponentProperties {
     name: string;
 }
 
@@ -8,12 +9,12 @@ interface FirstNavigationState {
     name: string;
 }
 
-class FirstNavigation extends React.Component<FirstNavigationParameters, FirstNavigationState> {
+export class FirstNavigation extends React.Component<FirstNavigationParameters, FirstNavigationState> {
 
     constructor(props: FirstNavigationParameters) {
         super(props);
         this.state = {
-        name: props.name
+            name: props.name
         };
     }
 
@@ -22,16 +23,19 @@ class FirstNavigation extends React.Component<FirstNavigationParameters, FirstNa
         console.log(this);
         console.log("on random prompt.");
         this.setState({ name: "George Lucas" })
+        this.props.dispatcher(new SetMainViewAction(MainViewType.RandomPrompt));
     }
 
     onViewPrompts = (e: React.MouseEvent) => {
         e.preventDefault();
         console.log("Show prompts");
+        this.props.dispatcher(new SetMainViewAction(MainViewType.ShowPrompts));
     }
 
     onViewWritingEntries = (e: React.MouseEvent) => {
         e.preventDefault();
-        console.log("view writing entries.")
+        console.log("view writing entries.");
+        this.props.dispatcher(new SetMainViewAction(MainViewType.ShowEntries));
     }
 
     render() {
@@ -48,5 +52,3 @@ class FirstNavigation extends React.Component<FirstNavigationParameters, FirstNa
         );
     }
 }
-
-export default FirstNavigation;
