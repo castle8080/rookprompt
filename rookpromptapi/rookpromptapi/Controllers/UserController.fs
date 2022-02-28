d274dfb4ce7ec8ac43583f2ce484939f12867422
Bryan Castillo <castillo.bryan@gmail.com>
2022-02-28 14:47:37 -0800

    Adding controller for user.

--- rookpromptapi/rookpromptapi/Controllers/UserController.fs
+++ rookpromptapi/rookpromptapi/Controllers/UserController.fs
@@ -12,6 +12,17 @@ open rookpromptapi
 open rookpromptapi.Models
 open rookpromptapi.Services
 
+module UserController =
+    // A separate request type is used to create the user to double check email
+    // and to accept credentials on creation.
+    type UserCreationRequest = {
+        Name : string
+        Email : string
+        EmailVerify : string
+        Password : string
+        PasswordVerify : string
+    }
+
 [<ApiController>]
 type UserController(
     logger : ILogger<UserController>,
@@ -22,3 +33,42 @@ type UserController(
     [<Route("users")>]
     member this.List() =
         userService.List() |> Async.StartAsTask
+
+    [<HttpPost>]
+    [<Route("users")>]
+    member this.Post([<FromBodyAttribute>] userRequest: UserController.UserCreationRequest) =
+        
+        // Basic validity check.
+        // Add better checks.
+        match userRequest with
+            | { Email = null } | { Email = "" } | { EmailVerify = null } | { EmailVerify = "" }
+                -> raise (InputValdationError "Missing email.")
+            | { Password = null } | { Password = "" } | { PasswordVerify = null } | { PasswordVerify = "" }
+                -> raise (InputValdationError "Missing password.")
+            | { Name = null } | { Name = "" }
+                -> raise (InputValdationError "Missing name.")
+            | _ when userRequest.Email <> userRequest.EmailVerify
+                -> raise (InputValdationError "Email does not match.")
+            | _ when userRequest.Password <> userRequest.PasswordVerify
+                -> raise (InputValdationError "Passwords do not match.")
+            | _ ->
+                ()
+
+        let user: User = {
+            Id = ""
+            Name = userRequest.Name
+            Email = userRequest.Email
+            Roles = []
+            Created = DateTime.UtcNow
+            Updated = DateTime.UtcNow
+        }
+
+        async {
+            logger.LogInformation($"Received new user: {user}")
+            let! user = userService.Create user
+            logger.LogInformation($"User created {user}")
+
+            // todo: setup credential
+
+            return user
+        }
--- rookpromptapi/rookpromptapi/Models/Errors.fs
+++ rookpromptapi/rookpromptapi/Models/Errors.fs
@@ -3,3 +3,4 @@
 exception DatabaseOperationFailure of string
 exception UserExists of string
 exception UserNotFound of string
+exception InputValdationError of string
